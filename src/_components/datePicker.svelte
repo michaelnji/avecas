cbfdb3f7e76e71ac70f3e43353a979a6986f4ca5
Michael <michaelnji34@gmail.com>
2021-06-25 19:14:28 +0100

    created dispatch event to send date upward to parent component

--- src/_components/datePicker.svelte
+++ src/_components/datePicker.svelte
@@ -1,11 +1,19 @@
 <script>
     import { DatePicker } from "attractions";
+    import { createEventDispatcher } from "svelte";
     export let top, right;
+    const dispatch = createEventDispatcher();
+    // send date up one level to editor and eventEditor component
+    function sendDate(e) {
+        const data = e.detail.value;
+        dispatch("chooseDate", data);
+    }
 </script>
 
 <div>
     <DatePicker
-        class="bg-red-200 text-black"
+        on:change={sendDate}
+        class="bg-base-200 text-black"
         weekdaysClass="text-primary"
         format="%m/%d/%Y"
         {top}
